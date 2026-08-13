96f881527a63a873a06a29a2fa7f2b21e9086495
PRAJWAL NAIK <chiefdraco101@gmail.com>
2026-08-13 23:17:44 +0530

    DAY1 commit2

---
+++ prog2/prog2.java
@@ -0,0 +1,23 @@
+package prog2;
+
+public class prog2
+{
+    public void main()
+    {
+        int x=10;
+        float y=22.3f;
+        double d=22.4;
+        char ch ='s';
+        boolean b=true;
+        String str="Her";
+
+        System.out.println("Integer Value: "+x);
+        System.out.println("Float Value: "+y);
+        System.out.println("Double Value: "+d);
+        System.out.println("Character Value: "+ch);
+        System.out.println("Boolean Value: "+b);
+        System.out.println("String Value: "+str);
+     
+
+    }
+}
