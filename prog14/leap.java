839c4f3b6bc295fe6ee24b229a71ad8b5be6ee93
PRAJWAL NAIK <chiefdraco101@gmail.com>
2026-08-17 01:00:42 +0530

    DAY4 commit 4

---
+++ prog14/leap.java
@@ -0,0 +1,27 @@
+package prog14;
+import java.util.Scanner;
+
+public class leap 
+{
+    public void main()
+    {
+    int year;
+    Scanner sc=new Scanner(System.in);
+
+    //input
+    year = sc.nextInt();
+
+    //process
+    if(year%4==0||year%100!=0&&year%400==0)
+    {
+        System.out.println("Year is leap year");
+    }
+    else
+    {
+        System.out.println("Year is not leap year");
+    }
+    sc.close();
+    }
+
+    
+}
