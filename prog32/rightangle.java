5c59fda039ebb15e4506c472598779f293055e0e
PRAJWAL NAIK <chiefdraco101@gmail.com>
2026-08-19 00:51:28 +0530

    DAY 7 commit 2

---
+++ prog32/rightangle.java
@@ -0,0 +1,16 @@
+package prog32;
+
+public class rightangle 
+{
+    public void main()
+    {
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    }
+}
